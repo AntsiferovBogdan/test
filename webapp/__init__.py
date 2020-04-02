b2422bfc27150920eddf2a0caa2e4954ae882bcb
Antsiferov Bogdan <58101453+AntsiferovBogdan@users.noreply.github.com>
2020-04-02 14:35:57 +0300

    delete migrations

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from flask_migrate import Migrate
 
 from webapp.db import db
 from webapp.user.views import blueprint as user_blueprint
@@ -9,7 +8,6 @@ def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
     db.init_app(app)
-    migrate = Migrate(app, db)
 
     app.register_blueprint(user_blueprint)
 
